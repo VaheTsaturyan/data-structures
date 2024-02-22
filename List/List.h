2b003f26750e7926b18efd8ab24a79d2038d3533
VaheTsaturyan <tsaturyan.1969@gmail.com>
2024-02-23 00:05:26 +0400

    v

--- List/List.h
+++ List/List.h
@@ -4,6 +4,9 @@
 namespace my{
 
 using Size = int;
+//using Iterator = typename List<T>::Iterator;
+//using Node = typename List<T>::Node;
+
 
 template<typename T>
 class List{
@@ -11,15 +14,31 @@ public:
 	List();
 	List(T data);
 	List(T* data);
-	List()
 	void push_front(T data);
 	void pop_front();
 	void push_back(T data);
 	void pop_back();
 	void insert(Size index, T data);
 	void erese(Size index);
-	Node& at(Size index);
+	T& at(Size index);
+	typename List<T>::Iterator begin();
+	typename List<T>::Iterator end();
 
+	
+	class Iterator{
+	public:	
+		Iterator(typename List<T>::Node* value);
+		~Iterator();
+		T& operator *();
+		void operator= (const Iterator& other);
+		my::List<T>::Iterator* operator ++();
+		my::List<T>::Iterator* operator --();
+		bool operator ==(const Iterator& other);
+		bool operator !=(const Iterator& other);
+
+	private:
+		typename List<T>::Node* ptr;
+	};
 
 
 private:
@@ -30,20 +49,43 @@ private:
 				this->l_ptr = l_ptr;
 				this->r_ptr = r_ptr;
 			} 
+			/*void operator=(const Node& other){
+				this->data = other.data;
+				this->l_ptr = other.l_ptr;
+				this->r_ptr = other.r_ptr;
+			}*/
 			T data;
-			Node l_ptr;
-			Npde r_ptr;
+			typename List<T>::Node* l_ptr;
+			typename List<T>::Node* r_ptr;
 
 		private:
 
 	};
 private:
 	Size size;
-	Node* head;
-	Node* tail;
+	typename List<T>::Node* head;
+	typename List<T>::Node* tail;
 
 };
 
+template<typename T>
+List<T>::List(){
+	this->head = this->tail = nullptr;
+	this->size = 0;
+}
+
+template<typename T>
+List<T>::List(T data){
+		this->head = new Node(data);
+		this->tail = this->head;
+		this->size = 1;
+}
+
+
+template<typename T>
+List<T>::List(T* data){
+	this->push_back(*data);
+}
 
 template<typename T>
 void List<T>::push_front(T data){
@@ -78,14 +120,14 @@ void List<T>::pop_front(){
 	size--;	
 }
 
-template<ttpename T>
+template<typename T>
 void List<T>::push_back(T data){
 	if(this->size == 0){
 		this->head = new Node(data);
 		this->tail = this->head;
 		this->size++;
-	}else (){
-		Node* tempNode = this->tail;
+	}else{
+		List<T>::Node* tempNode = this->tail;
 		this->tail->r_ptr = new Node(data);
 		this->tail = this->tail->r_ptr;
 		this->tail->l_ptr = tempNode;
@@ -119,18 +161,19 @@ void List<T>::insert(Size index, T data){
 	}else if(index == 0){
 		push_front(data);
 	}else  if(index < this->size/2){
-		Node* tempNode = this->head;
+		List<T>::Node* tempNode = this->head;
 		for(int i = 0; i < index; ++i){
 			tempNode = tempNode->r_ptr;
 		}
-		tempNode->r_ptr = new Node(data,tempNode,tempNode->r_ptr)
+		tempNode->r_ptr = new Node(data,tempNode,tempNode->r_ptr);
 	}else{
-		Node* tempNode = this->tail;
+		List<T>::Node* tempNode = this->tail;
 		for(int i = this->size; i > index; --i){
 			tempNode = tempNode->l_ptr;
 		}
-		tempNode->l_ptr = new Node(data,tempNode->l_ptr;tempNode);
+		tempNode->l_ptr = new Node(data,tempNode->l_ptr,tempNode);
 	}
+}
 
 
 template<typename T>
@@ -151,32 +194,90 @@ void List<T>::erese(Size index){
 }
 
 
-
-Node& at(Size index){
-	if(index > this->size|| index < 0 ){
-		return;
+template<typename T>
+T& List<T>::at(Size index){
+	if(index >= this->size|| index < 0 ){
+		throw std::runtime_error("index unavailable");
 	}else if(index == this->size- 1){
-		return *this->tail;
+		return this->tail->data;
 	}else if(index == 0){
-		return *this->head;
+		return this->head->data;
 	}else  if(index < this->size/2){
-		Node* tempNode = this->head;
+		List<T>::Node* tempNode = this->head;
 		for(int i = 0; i < index; ++i){
 			tempNode = tempNode->r_ptr;
 		}
-		return *tempNode;
+		return tempNode->data;
 	}else{
-		Node* tempNode = this->tail;
+		List<T>::Node* tempNode = this->tail;
 		for(int i = this->size; i > index; --i){
 			tempNode = tempNode->l_ptr;
 		}
-		return *tempNode;
+		return tempNode->data;
 	}
 }
 
-
-
+template<typename T>
+typename List<T>::Iterator List<T>::begin(){
+	return my::List<T>::Iterator(this->head);
 }
+
+
+template<typename T>
+typename List<T>::Iterator List<T>::end(){
+	return my::List<T>::Iterator((nullptr));;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+List<T>::Iterator::Iterator(List<T>::Node* value){
+	this->ptr = value;
+}
+
+template<typename T>
+List<T>::Iterator::~Iterator(){
+	this->ptr = nullptr;
+}
+
+template<typename T>
+T& List<T>::Iterator::operator *(){
+	return this->ptr->data;
+}
+
+template<typename T>
+void List<T>::Iterator::operator= (const Iterator& other){
+	this->ptr->data = other.ptr->data;
+	this->ptr->l_ptr = other.ptr->l_ptr;
+	this->ptr->r_ptr = other.ptr->r_ptr;
+	
+}
+
+template<typename T>
+typename List<T>::Iterator* List<T>::Iterator::operator ++(){
+	this->ptr = this->ptr->r_ptr;
+	return this;
+}
+
+template<typename T>
+typename List<T>::Iterator* List<T>::Iterator::operator --(){
+	this->ptr = this->ptr->l_ptr;
+	return this;
+}
+
+template<typename T>
+bool List<T>::Iterator::operator ==(const Iterator& other){
+	return this == &other;
+}
+
+template<typename T>
+bool List<T>::Iterator::operator !=(const Iterator& other){
+	return this != &other;
+}
+
+
+
 
 }// my
 
--- List/main.cpp
+++ List/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
-#include <List.h>
+#include "List.h"
 
 
 int main(){
-	my::
+	my::List<int> lst;
+	lst.push_back(0);
+	lst.push_back(1);
+	lst.push_back(2);
+	lst.push_back(3);
+	lst.push_back(4);
+	lst.push_back(5);
+	lst.push_back(6);
+	//lst.pop_front();
+	//lst.pop_back();
+	lst.insert(3,10);
+	//lst.push_front(10);
+	my::List<int>::Iterator it = lst.begin();
+	std::cout<<"===================================\n";
+	std::cout<<lst.at(3);
+	while(it != lst.end()){
+		std::cout<<*it<<std::endl;
+		++it;
+	}
 
 	return 0;	
 }
